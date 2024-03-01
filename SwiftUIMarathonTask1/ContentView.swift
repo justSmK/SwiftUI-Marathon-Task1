5b0ebb6b6f6ea96c51fc26cbf47fe6af60240799
justSmK <28645140+justSmK@users.noreply.github.com>
2024-03-01 13:15:42 +0300

    Task 1 Implementation

--- SwiftUIMarathonTask1/ContentView.swift
+++ SwiftUIMarathonTask1/ContentView.swift
@@ -7,15 +7,38 @@
 
 import SwiftUI
 
+fileprivate enum Constants {
+    static let redRectangleHeight: CGFloat = 50
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            TabView {
+                ScrollView {
+                    VStack {
+                        ForEach(1..<101) { item in
+                            Text("\(item)")
+                                .font(.system(size: 35))
+                                .fontWeight(.regular)
+                                .foregroundStyle(.gray)
+                                .frame(
+                                    width: geometry.size.width,
+                                    alignment: .center
+                                )
+                        }
+                    }
+                }
+                .tabItem {
+                    Label("First", systemImage: "star.fill")
+                }
+                .safeAreaInset(edge: .bottom) {
+                    Rectangle()
+                        .foregroundStyle(.red.opacity(0.8))
+                        .frame(height: Constants.redRectangleHeight)
+                }
+            }
         }
-        .padding()
     }
 }
 
